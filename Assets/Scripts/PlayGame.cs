b82f86f3de932c85b3f90205d9e5034f6854c6e0
Roman Sharf <pencilking2002@gmail.com>
2018-01-25 20:56:04 -0500

    main menu button script

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour 
+{
+    void Start()
+    {
+        Button b = GetComponent<Button>();
+        b.onClick.AddListener(() =>
+        {
+            Manager.Instance.Game.StartGame();
+        });
+    }
+
+}
