a8a0b8c10a84e9314ecad7665b5b951150a9ac7c
Roman Sharf <pencilking2002@gmail.com>
2018-01-25 20:43:07 -0500

    - Added LeanTween - Added Managers object

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public static Manager Instance;
+    public GameManager Game;
+
+    void Awake()
+    {
+        if (InitInstance())
+        {
+            DontDestroyOnLoad(gameObject);
+            Game = GetComponentInChildren<GameManager>();
+        }
+    }
+
+    bool InitInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            return true;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return false;
+        }
+    }
+
+
+   
+
+}
