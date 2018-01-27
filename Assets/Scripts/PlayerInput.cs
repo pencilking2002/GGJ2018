52b9ec47931fe387ec0e3e5e34aa263cdbc7c623
Roman <pencilking2002@gmail.com>
2018-01-27 12:13:33 -0500

    Added support for keyboard only

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -10,18 +10,32 @@ public class PlayerInput : MonoBehaviour {
 
 	void Update()
 	{
-        InputDevice device = InputManager.ActiveDevice;
+		
+        //InputDevice device = InputManager.ActiveDevice;
+
+
+
         //InputControl control = device.GetControl(InputControlType.Action1);
-        var player1 = InputManager.Devices[0];
-        var player2 = InputManager.Devices[1];
+        if (InputManager.Devices.Count != 0)
+        {
+	        var player1 = InputManager.Devices[0];
+	        var player2 = InputManager.Devices[1];
 
-        //      x = Input.GetAxis("Horizontal");
-        //y = Input.GetAxis("Vertical");
-        x = player1.LeftStick.Value.x;
-        y = player1.LeftStick.Value.y;
 
-        Debug.Log("Player 1 is a  : " + player1.Name);
-        Debug.Log("Player 2 is a  : " + player2.Name);
+	        //      x = Input.GetAxis("Horizontal");
+	        //y = Input.GetAxis("Vertical");
+	        x = player1.LeftStick.Value.x;
+	        y = player1.LeftStick.Value.y;
+
+	        Debug.Log("Player 1 is a  : " + player1.Name);
+	        Debug.Log("Player 2 is a  : " + player2.Name);
+        }
+        else
+        {
+
+			x = Input.GetAxis("Horizontal");
+			y = Input.GetAxis("Vertical");
+        }
 	}
 
     public Vector3 GetPlayerInput(int playerIndex)
