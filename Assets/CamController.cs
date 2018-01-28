d28ede184d83d68997687766ac4cde269df43b3f
Roman <pencilking2002@gmail.com>
2018-01-28 14:19:07 -0500

    GAME OVER

--- Assets/CamController.cs
+++ Assets/CamController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamController : MonoBehaviour {
+	public Vector3 origPos;
 
 	public float numMoveDown = -10;
 
 	void Start()
 	{
+		origPos = transform.position;
 		//print ("start");
 	}
 
@@ -27,5 +29,11 @@ public class CamController : MonoBehaviour {
 	void OnDisable()
 	{
 		GameManager.onStartMatch -= MoveCamToMatchPos;
+	}
+
+	public void ResetPos()
+	{
+		transform.position = origPos;
+		PerlinShake.Instance.AssignOrigPos();
 	}
 }
